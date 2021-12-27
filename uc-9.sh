a6357bfcdb571572030ee978dd9f713f700e7699
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2021-12-27 20:38:13 +0530

    Add files via upload

---
+++ uc-9.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=100
+empRatePerHr=20
+numOfWorkingDays=20
+
+totalEmpHr=0
+totalWorkigDays=0
+
+declare -A dailyWages;
+function getWorkHrs(){
+      case $1 in
+                 $isFullTime)
+                       empHrs=8
+                       ;;
+                 $isPartTime)
+                       empHrs=4
+                       ;;
+                  *)
+                       empHrs=0
+                       ;;
+     esac
+}
+
+function getEmpWage(){
+                   echo $(($1*$empRatePerHr))
+}
+
+while [[ $totalEmpHr -lt $maxHrsInMonth &&
+                       $totalWorkingDays -lt $numOfWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        randomCheck=$((RANDOM%3))
+        getWorkHrs $randomCheck
+        totalEmpHr=$(($totalEmpHr+$empHrs))
+        dailyWages["$totalWorkingDays"]=$(($empHrs*$empRatePerHr))
+done
+
+      totalSalary="$(getEmpWage $totalEmpHr)"
+      echo ${dailyWages[@]} #using array
+
+      echo ${!dailyWages[@]} #using dictionary
